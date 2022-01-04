f12b133b0c8fd929227e71eea08a7bb62ec2f824
amitleuva1987 <amitleuva1987@gmail.com>
2022-01-04 11:22:53 +0530

    screenshots and view updated

--- resources/views/users/following.blade.php
+++ resources/views/users/following.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Users') }}
+            {{ __('Following Users') }}
         </h2>
     </x-slot>
 
